e7517fa6782d6d4838f8fd529e3feb25624f369e
xijun.lee <xijun.lee@hotmail.com>
2017-07-06 15:23:43 +0800

    modified the code

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import numpy as np
 #from pandas import *
 import xgboost as xgb
 import pdb
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import normalize
 
 trainX_file_paths = ['../data/train/12/12_data.csv','../data/train/23/23_data.csv','../data/train/29/29_data.csv']
 trainY_file_paths = ['../data/train/12/12_failureInfo.csv','../data/train/23/23_failureInfo.csv','../data/train/29/29_failureInfo.csv']
@@ -50,20 +53,42 @@ for k in xrange(3):
         if trainX_df.iat[i,0] >= t0:
             positive_ed = i
             break
-
+    
+    if positive_st != -1:
+        '''
+        phase1 = np.linspace(0,0.5*100,positive_st)
+        phase2 = np.array([0.5*100 for i in xrange(highRisk_st-positive_st)])
+        phase3 = np.array([0.8*100 for i in xrange(highRisk_ed-highRisk_st)])
+        phase4 = np.linspace(0.8*100,1*100,trainX_df.shape[0]-highRisk_ed)
+        '''
+        
+        phase1 = np.array([0.2*100 for i in xrange(positive_st)])
+        phase2 = np.array([0.5*100 for i in xrange(highRisk_st-positive_st)])
+        phase3 = np.array([0.8*100 for i in xrange(highRisk_ed-highRisk_st)])
+        phase4 = np.array([1 for i in xrange(trainX_df.shape[0]-highRisk_ed)])
+        trainY = np.concatenate((phase1,phase2,phase3,phase4), axis=0)
+    else:
+        trainY = np.array([40 for i in xrange(trainX_df.shape[0])])
+    '''
     trainY = []
     for i in xrange(trainX_df.shape[0]):
         if i >= positive_st and i <= positive_ed:
-            if i >= highRisk_st:
-                trainY.append(2)
-            else:
+            #if i >= highRisk_st:
+            #    trainY.append(2)
+            #else:
                 trainY.append(1)
         else:
             trainY.append(0)
     trainY = np.array(trainY)
-
+    '''
+    
     trainX_df = trainX_df.drop(['time'],axis=1)
     trainX = trainX_df.values
+    trainX = normalize(trainX,norm='l2',axis=1)
+    
+    print trainX,trainY
+    print trainX.shape, trainY.shape
+
     if k == 0:
         trainX_concate, trainY_concate = trainX, trainY
     else:
@@ -90,6 +115,11 @@ print res
 
 print 'training xgbclassfier ...'
 
+nComponent = 20
+pca = PCA(n_components=nComponent)
+pca.fit(trainX_concate)
+trainX_concate = pca.transform(trainX_concate)
+
 clf = xgb.XGBClassifier(
     #learning_rate = 0.02,
  n_estimators= 500,
@@ -112,20 +142,24 @@ for k in xrange(len(testX_file_paths)):
     testX = testX_pd.drop(['time'], axis=1).values
 
     print 'predicting the %d th test dataset ...'%k
+    
+    pca = PCA(n_components=nComponent)
+    pca.fit(testX)
+    testX = pca.transform(testX)
     predictions = clf.predict(testX)
 
     print 'generating submission file of the %d test dataset ...'%k
     # Generate Submission File 
     st, ed = -1, -1
     for i in xrange(len(predictions)):
-        if predictions[i] == 1:
+        if predictions[i] >= 50.0:
             st = i
             break
     for i in xrange(len(predictions)-1,-1,-1):
-        if predictions[i] == 1:
+        if predictions[i] <= 80.0:
             ed = i
             break
-    t1, t2 = [st+1], [ed+1]
+    t2, t1 = [st+1], [ed+1]
     mid_result = pd.DataFrame({'predictions':predictions})
     mid_result.to_csv('test_'+str(k)+'mid_result.csv',index=True)
     sub = pd.DataFrame({'t1':t1,'t2':t2})
